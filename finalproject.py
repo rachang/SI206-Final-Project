22cf58c6ea58d2eeb08cd55f00742b77f212a9c6
rachang <rachang@umich.edu>
2017-12-07 13:45:35 -0500

    Third Commit

--- finalproject.py
+++ finalproject.py
@@ -8,6 +8,7 @@ import sqlite3
 import facebook
 import api_access
 import requests
+from datetime import datetime
 CACHE_FNAME = "finalproject.json"
 # Put the rest of your caching setup here:
 try:
@@ -17,8 +18,10 @@ try:
     CACHE_DICTION = json.loads(cache_contents)# And then load it into a dictionary
 except:
     CACHE_DICTION = {}
+
 fb_access_token = api_access.fb_access_token
 fb_user = api_access.fb_user
+
 def get_Facebook(user):
 	fb_url = "https://graph.facebook.com/v2.11/" + fb_user + "/feed"
 	url_params = {}
@@ -37,5 +40,30 @@ def get_Facebook(user):
 		fw.close() # Close the open file
 	# return data that was retrieved from Twitter. 
 	return results
-hello = get_Facebook(fb_user)
+fbposts = get_Facebook(fb_user)
 
+conn = sqlite3.connect('finalproject.sqlite')
+cur = conn.cursor()
+cur.execute("DROP TABLE IF EXISTS Facebook_Posts")
+
+cur.execute('''CREATE TABLE Facebook_Posts (user_id TEXT, created_time DATETIME, weekday TEXT, message TEXT, story TEXT)''')
+for posts in fbposts["data"]:
+	user_id = posts["id"]
+	created_time = posts["created_time"]
+	created_time = datetime.strptime(created_time, "%Y-%m-%dT%H:%M:%S%z")
+	weekday = datetime.strftime(created_time,"%A")
+	if "story" in posts:
+		story = posts["story"]
+	else:
+		story = ""
+	if "message" in posts:
+		message = posts["message"]
+	else:
+		message = ""
+	print(created_time, weekday)
+
+
+	cur.execute('''INSERT or IGNORE INTO Facebook_Posts (user_id, created_time, weekday, message, story) VALUES (?,?,?,?,?)'''
+		, (user_id, created_time, weekday, message, story))
+conn.commit()
+cur.close()
